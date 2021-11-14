294d9f425fbfcad4c0c030711f73128b4ed364a5
Sam Richardson <sam@richardson.co.nz>
2021-11-14 21:11:08 +1100

    Added stop image

--- app/pages/index.js
+++ app/pages/index.js
@@ -2,14 +2,18 @@ import React from "react";
 import Head from 'next/head'
 import VideoJS from 'components/VideoJS/VideoJS.jsx'
 import VideoMeta from 'components/VideoMeta/VideoMeta.jsx'
+import Image from 'next/image'
 
 import styles from '../styles/Home.module.css'
 
 const FRAME_RATE = 30;
+const DEFAULT_STOP_ON_FRAME = 78;
 
 export default function Home() {
   const playerRef = React.useRef(null);
+  const stopOnFrameRef = React.useRef(DEFAULT_STOP_ON_FRAME);
   const [currentFrame, setCurrentFrame] = React.useState(null);
+  const [stopOnFrame, setStopOnFrame] = React.useState(DEFAULT_STOP_ON_FRAME);
 
   const videoJsOptions = { // lookup the options in the docs for more options
     autoplay: false,
@@ -37,9 +41,10 @@ export default function Home() {
     const frameCounter = (time, metadata) => {
       const count = metadata.mediaTime * FRAME_RATE;
       const roundedCount = Math.round(count);
-      console.log(roundedCount)
 
-      if (roundedCount === 78) {
+      console.log(roundedCount, stopOnFrameRef.current)
+
+      if (roundedCount === stopOnFrameRef.current) {
         player.pause();
       }
 
@@ -57,6 +62,12 @@ export default function Home() {
 
   const handleForwards = () => {
     playerRef.current.currentTime(playerRef.current.currentTime() + 1 / FRAME_RATE);
+  }
+
+  const handleChangeStopOnFrame = (event) => {
+    const value = event.target.value;
+    setStopOnFrame(value);
+    stopOnFrameRef.current = parseInt(value, 10);
   }
 
   React.useEffect(() => {
@@ -92,6 +103,15 @@ export default function Home() {
             <button onClick={handleBack}>Back</button>
             <button onClick={handleForwards}>Forward</button>
           </div>
+          <h2>Stop on frame</h2>
+          <p>Set the frame to stop on</p>
+          <input type="number" min="0" step="1" onChange={handleChangeStopOnFrame} value={stopOnFrame} />
+          <h2>Stopping on frame 78 should match</h2>
+          <Image
+            src="/match.png"
+            width={259}
+            height={478}
+          />
         </aside>
       </main>
     </>
